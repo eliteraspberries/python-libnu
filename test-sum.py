d1c098682a1a069745acb4e059e2542a0a159cd1
Mansour Moufid <mansourmoufid@gmail.com>
2018-11-23 20:55:24 -0500

    Use property-based testing.

--- test-sum.py
+++ test-sum.py
@@ -3,14 +3,20 @@
 import functools
 import numpy
 
+import hypothesis
+from hypothesis.extra.numpy import arrays
+from hypothesis.strategies import floats
+
 import libnu.sum
+
+from test import eq
 
 numpy.zeros = functools.partial(numpy.zeros, dtype=numpy.float32)
 
 
-def test_sum():
-    x = numpy.zeros(1000)
-    assert libnu.sum.sum(x) == 0.0
+@hypothesis.given(arrays(numpy.float32, 10, elements=floats(0.0, 1.0)))
+def test_sum(x):
+    assert eq(libnu.sum.sum(x), numpy.sum(x), 1e-6)
 
 
 if __name__ == '__main__':
